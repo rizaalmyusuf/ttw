5f21abd4232748f1a425def7f0600da31e76435c
Rizal M. Yusuf <rijalmy010600@gmail.com>
2025-03-18 22:00:26 +0700

    Implement Admin Panel with custom login page; refactor User model and update providers for Filament integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -19,13 +18,15 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'username',
-        'name',
-        'email',
-        'password',
-        'role',
-    ];
+    // protected $fillable = [
+    //     'username',
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'role',
+    // ];
+
+    protected $guarded;
 
     /**
      * The attributes that should be hidden for serialization.
