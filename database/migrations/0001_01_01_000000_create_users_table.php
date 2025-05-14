86bee10c6ffaff27ca1d0bba447507cb43e1f44e
Rizal M. Yusuf <rijalmy010600@gmail.com>
2025-05-14 18:57:07 +0700

    Update default user role to 2; comment out student_id foreign key in classrooms migration; fix drop table name in classroomables migration.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('role')->default(0);
+            $table->tinyInteger('role')->default(2);
             $table->boolean('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
--- database/migrations/2025_03_20_130108_create_classrooms_table.php
+++ database/migrations/2025_03_20_130108_create_classrooms_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('subject');
             $table->foreignId('teacher_id')->constrained('users');
-            $table->foreignId('student_id')->nullable()->constrained('users');
+            // $table->foreignId('student_id')->nullable()->constrained('users');
             $table->timestamps();
         });
     }
