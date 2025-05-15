ea65b8f358907c29fde9a51da8fe375bc4d2f8af
Rizal M. Yusuf <rijalmy010600@gmail.com>
2025-05-15 19:43:26 +0700

    Refactor RegistrationApp: username exist error message, remove unused registration logic and streamline form component

--- app/Filament/Pages/Auth/RegistrationApp.php
+++ app/Filament/Pages/Auth/RegistrationApp.php
@@ -3,61 +3,10 @@
 namespace App\Filament\Pages\Auth;
 
 use Filament\Forms;
-use Filament\Facades\Filament;
 use Filament\Pages\Auth\Register;
-use Filament\Events\Auth\Registered;
-use Filament\Http\Responses\Auth\Contracts\RegistrationResponse;
-use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
-use Filament\Notifications\Notification;
 
 class RegistrationApp extends Register
 {
-    public function register(): ?RegistrationResponse
-    {
-        try {
-            $this->rateLimit(2);
-        } catch (TooManyRequestsException $exception) {
-            $this->getRateLimitedNotification($exception)?->send();
-
-            return null;
-        }
-
-        $user = $this->wrapInDatabaseTransaction(function () {
-            $this->callHook('beforeValidate');
-
-            $data = $this->form->getState();
-
-            $this->callHook('afterValidate');
-
-            $data = $this->mutateFormDataBeforeRegister($data);
-
-            $this->callHook('beforeRegister');
-
-            $user = $this->handleRegistration($data);
-
-            $this->form->model($user)->saveRelationships();
-
-            $this->callHook('afterRegister');
-
-            return $user;
-        });
-
-        event(new Registered($user));
-
-        $this->sendEmailVerificationNotification($user);
-
-        Filament::auth()->login($user);
-
-        session()->regenerate();
-
-        return app(RegistrationResponse::class);
-    }
-
-    public function form(Forms\Form $form): Forms\Form
-    {
-        return $form;
-    }
-
     protected function getForms(): array
     {
         return [
@@ -81,6 +30,7 @@ class RegistrationApp extends Register
             ->label('Username')
             ->required()
             ->maxLength(255)
-            ->autofocus();
+            ->autofocus()
+            ->unique($this->getUserModel());;
     }
 }
