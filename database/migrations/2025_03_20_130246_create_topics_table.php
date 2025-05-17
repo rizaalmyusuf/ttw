bb2e6fac3680b92fc206f90c27a0e6709996933c
Rizal M. Yusuf <rijalmy010600@gmail.com>
2025-05-17 12:12:08 +0700

    Refactor topics migration: change description column type from string to text for improved data handling

--- database/migrations/2025_03_20_130246_create_topics_table.php
+++ database/migrations/2025_03_20_130246_create_topics_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->binary('file');
             $table->foreignId('classroom_id')->constrained('classrooms');
             $table->timestamps();
